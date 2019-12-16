141f50faa1378ee95664c802acddf9d0b3c6bdb6
Kledon Pug <andrewpan31@gmail.com>
2019-12-16 14:27:59 +0200

    Lab2

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,34 @@
+﻿//Обчислити суму числового ряду при 0 <= nn <= nk
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nn, nk, k, i;
+            float S = 0;
+            Console.WriteLine("Важно nn >= 0 , nn <= nk");
+            do
+            {
+                Console.WriteLine("Введите nn");
+                nn = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите nk");
+                nk = Convert.ToInt32(Console.ReadLine());
+            } while (nk < nn || nn < 0);
+            k = nn;
+            for (i = 0; i < nk - nn; i++)
+            {
+                k = k + 1;
+                S = S + (((k * k) * (k * k * k)) / (k * k + k + 1));
+                Console.WriteLine("-");
+            }
+            Console.WriteLine("S = ");
+            Console.WriteLine(S);
+        }
+    }
+}
