991d50956fc84a45349492e994553a90dbd255ff
Kledon Pug <andrewpan31@gmail.com>
2019-12-16 14:24:51 +0200

    Lab3_in_VS

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, S;
+            Console.WriteLine("Введите А");
+            A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите B");
+            B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите C");
+            C = Convert.ToInt32(Console.ReadLine());
+            S = B * B - 4 * A * C;
+            if (S > 0)
+            {
+                Console.WriteLine("Уравнение имеет 2 кореня");
+            }
+            else { Console.WriteLine("Уравнение не имеет 2 кореня"); }
+        }
+    }
+}
