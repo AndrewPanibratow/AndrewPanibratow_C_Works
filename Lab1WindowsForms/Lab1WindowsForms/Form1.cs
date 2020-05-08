6b208d91b61be0c7dd6087640fa155b9430ff437
Kledon Pug <andrewpan31@gmail.com>
2020-05-08 15:16:47 +0300

    Lab1WindowsForms(Kalculahtor)

---
+++ Lab1WindowsForms/Lab1WindowsForms/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        double a, b, c;
+        char znak = '+';
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            znak = (sender as Button).Text[0];
+            textBox1.Clear();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            b = Convert.ToDouble(textBox1.Text);
+            switch(znak)
+            {
+                case '+': c = a + b;
+                    break;
+                case '-':
+                    c = a - b;
+                    break;
+                case '*':
+                    c = a * b;
+                    break;
+                case '/':
+                    c = a / b;
+                    break;
+            }
+            textBox1.Text = c.ToString();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != "")
+            {
+                if (textBox1.Text[0] == '-')
+                {
+                    textBox1.Text = textBox1.Text.Remove(0, 1);
+                }
+                else textBox1.Text = '-' + textBox1.Text;
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += (sender as Button).Text;
+            
+        }
+
+    }
+}
